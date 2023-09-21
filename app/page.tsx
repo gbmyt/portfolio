061758684e079e00ac85a133561cb7aea5415a03
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-09-21 12:00:58 -0400

    Replaced blog on home page with About Me description and resume

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,5 @@ import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  return <Main />
 }
