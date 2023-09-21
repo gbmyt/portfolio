c98154c556ac9b0e4ad73250be0b586c1c987469
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-09-21 12:00:31 -0400

    Removed extraneous About page and nav link

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,22 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from './Image'
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
+        {/* <div className="flex items-center"> */}
+        {/* <Image
+          src="/static/images/profile-img.jpeg"
+          alt="avatar"
+          width={192}
+          height={192}
+          className="h-10 w-10 rounded-full object-cover"
+        /> */}
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
