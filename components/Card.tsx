7623329ca7e1af67fd37843dae6fb556626c1f2a
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-09-28 13:44:24 -0400

    Updated image component to include unoptimized properly if the image if a gif. Otherwise does not include unoptimized={true}, left the Image component as it was when the template was downloaded.

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,18 +8,19 @@ const Card = ({ title, description, imgSrc, href, repository }) => (
         imgSrc && 'h-full'
       }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
-      {imgSrc &&
-        (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          </Link>
-        ) : (
+      {imgSrc && imgSrc.substr(imgSrc.length - 4) === '.gif' && href ? (
+        <Link href={href} aria-label={`Link to ${title}`}>
+          <Image
+            unoptimized={true}
+            alt={title}
+            src={imgSrc}
+            className="object-cover object-center md:h-36 lg:h-48"
+            width={544}
+            height={306}
+          />
+        </Link>
+      ) : imgSrc && href ? (
+        <Link href={href} aria-label={`Link to ${title}`}>
           <Image
             alt={title}
             src={imgSrc}
@@ -27,7 +28,16 @@ const Card = ({ title, description, imgSrc, href, repository }) => (
             width={544}
             height={306}
           />
-        ))}
+        </Link>
+      ) : (
+        <Image
+          alt={title}
+          src={imgSrc}
+          className="object-cover object-center md:h-36 lg:h-48"
+          width={544}
+          height={306}
+        />
+      )}
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
