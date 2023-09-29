f240656f0a0469dc2c66b254517ff72a600b6dd0
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-09-29 14:41:11 -0400

    Replaced Blog Template descirption with portfolio description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Gabriela Taylor | Portfolio',
   author: 'Gabriela Taylor',
   headerTitle: 'Gabriela Taylor',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Gabriela Taylor | Full Stack Software Engineer',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
