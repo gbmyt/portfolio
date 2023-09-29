afbb16b4b79b02951b82f1af45b1cca40dfe5721
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-09-29 14:29:08 -0400

    Fixed Typescript build errors and press escape to close resume modal

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,43 +1,60 @@
-"use client"
-import { useEffect, useState } from 'react';
+'use client'
+import { useCallback, useEffect, useState } from 'react'
 import '/css/modal.css'
 
 import resumeData from '@/data/resumeData'
-import ReactModal from 'react-modal';
+import ReactModal from 'react-modal'
 import Image from './Image'
 
 export default function About() {
   const [modalIsOpen, setOpen] = useState(false)
 
+  useEffect(() => {
+    ReactModal.setAppElement('body')
+
+    const modal = document.getElementById('modal')
+    modal?.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        setOpen(false)
+      }
+    })
+  }, [])
+
   function handleClick() {
     setOpen(!modalIsOpen)
-  }  
-  
-  useEffect(() => {
-    ReactModal.setAppElement('body');
+  }
 
-    function closeModal(e) {
-      if(e.keyCode === 27){
-        setOpen(!modalIsOpen)
-      }
-    }
-    window.addEventListener('keydown', closeModal)
-    return () => window.removeEventListener('keydown', closeModal)
-  }, [])
-  
+  function handleFocusModal() {
+    const modal = document.getElementById('modal')
+    modal?.focus()
+  }
+
   return (
     <>
       <h2 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">Resume</h2>
 
-      <div onClick={handleClick}>
-        <Image priority src={resumeData.imgSrc} alt="Resume" className=" border-solid border-2 border-gray-300" width="1200" height="1700" />
+      <div tabIndex={-1} id="modal" onClick={handleClick} role="presentation">
+        <Image
+          priority
+          src={resumeData.imgSrc}
+          alt="Resume"
+          className=" border-solid border-2 border-gray-300"
+          width="1200"
+          height="1700"
+        />
 
-        <ReactModal id='mopen' isOpen={modalIsOpen}>
-            <Image priority src={resumeData.imgSrc} alt="Resume" className="border-solid border-2 border-red" width="1200" height="1700" />
+        <ReactModal onAfterOpen={handleFocusModal} id="mopen" isOpen={modalIsOpen}>
+          <Image
+            priority
+            onKeyDown={(e) => e.key === 'Escape' && setOpen(false)}
+            src={resumeData.imgSrc}
+            alt="Resume"
+            className="border-solid border-2 border-red"
+            width="1200"
+            height="1700"
+          />
         </ReactModal>
-
       </div>
-
     </>
   )
 }
