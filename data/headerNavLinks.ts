c1ae15c2316d3b64ebb3f524ced2db1721e0b1e8
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-10-14 17:52:14 -0400

    - Removed the About page and associated author profile. - Edited the Nav Bar to remove the About page link. - Added .env to .gitignore

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
   { href: '/projects', title: 'Projects' },
 ]
 
